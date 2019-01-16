88e3f3f4c38d3b9577031bd3548ea39a12794597
Ted Shaffer <ted@brightsign.biz>
2019-01-16 15:38:55 -0800

    tried axios, failed: getting albums properly with request-promise

--- controllers/syncerController.js
+++ controllers/syncerController.js
@@ -1,8 +1,9 @@
 const requestPromise = require('request-promise');
+const axios = require('axios');
 
 var oauth2Controller = require('./oauth2Controller');
 
-exports.startSync = function(request, response, next) {
+exports.startSync = function (request, response, next) {
   response.render('syncer');
 
   var access_token = oauth2Controller.getAccessToken();
@@ -13,12 +14,35 @@ exports.startSync = function(request, response, next) {
 
   console.log('invoke: ', apiEndpoint + '/v1/albums');
 
+  // https://stackoverflow.com/questions/40988238/sending-the-bearer-token-with-axios
+  // axios.get(apiEndpoint + '/v1/albums',
+  // {
+  //   headers: { 
+  //     'Content-Type': 'application/json',
+  //     'Authorization': "bearer " + access_token,    
+  //   },
+  // }).then((result) => {
+  //   console.log(result);
+  // }).catch((err) => {
+  //   console.log(err);
+  // });
+
   requestPromise.get(apiEndpoint + '/v1/albums', {
-    headers: {'Content-Type': 'application/json'},
+    headers: { 'Content-Type': 'application/json' },
     json: true,
-    auth: {'bearer': access_token},
-  }).then( (result) => {
+    auth: { 'bearer': access_token },
+  }).then((result) => {
     console.log(result);
+
+    // result.albums is an array of albums
+    // each album object
+    //    id: string
+    //    mediaItemsCount: string
+    //    title: string
+    //    productUrl: string
+    //      not clear what would be returned using this url
+
+
   });
 
 }
