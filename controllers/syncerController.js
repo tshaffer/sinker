c86c6c81ea4e8f25d20f71b4a07db3209985d304
Ted Shaffer <ted@brightsign.biz>
2019-01-16 16:38:37 -0800

    retrieve all media items

--- controllers/syncerController.js
+++ controllers/syncerController.js
@@ -1,5 +1,5 @@
 const requestPromise = require('request-promise');
-const axios = require('axios');
+// const axios = require('axios');
 
 var oauth2Controller = require('./oauth2Controller');
 
@@ -12,37 +12,52 @@ exports.startSync = function (request, response, next) {
 
   const apiEndpoint = 'https://photoslibrary.googleapis.com';
 
-  console.log('invoke: ', apiEndpoint + '/v1/albums');
+  console.log('invoke: ', apiEndpoint + '/v1/mediaItems');
 
-  // https://stackoverflow.com/questions/40988238/sending-the-bearer-token-with-axios
-  // axios.get(apiEndpoint + '/v1/albums',
-  // {
-  //   headers: { 
-  //     'Content-Type': 'application/json',
-  //     'Authorization': "bearer " + access_token,    
-  //   },
+  var totalNumberOfMediaItems = 0;
+  var processGetMediaFiles = (pageToken) => {
+    var url = apiEndpoint + '/v1/mediaItems?pageSize=100'
+    if (pageToken !== '') {
+      url = url + '&pageToken=' + pageToken;
+    }
+    requestPromise.get(url, {
+      headers: { 'Content-Type': 'application/json' },
+      json: true,
+      auth: { 'bearer': access_token },
+    }).then((result) => {
+      console.log(result.mediaItems.length);
+      console.log(result.nextPageToken);
+
+      if (result.mediaItems.length === 0 || result.nextPageToken === undefined) {
+        console.log('retrieved all mediaItems');
+        debugger;
+      }
+      else {
+        totalNumberOfMediaItems = totalNumberOfMediaItems + result.mediaItems.length;
+        console.log('running total is: ', totalNumberOfMediaItems);
+        processGetMediaFiles(result.nextPageToken);
+      }
+    });
+  };
+
+  processGetMediaFiles('');
+
+  // requestPromise.get(apiEndpoint + '/v1/albums', {
+  //   headers: { 'Content-Type': 'application/json' },
+  //   json: true,
+  //   auth: { 'bearer': access_token },
   // }).then((result) => {
   //   console.log(result);
-  // }).catch((err) => {
-  //   console.log(err);
+
+  //   // result.albums is an array of albums
+  //   // each album object
+  //   //    id: string
+  //   //    mediaItemsCount: string
+  //   //    title: string
+  //   //    productUrl: string
+  //   //      not clear what would be returned using this url
+
+
   // });
-
-  requestPromise.get(apiEndpoint + '/v1/albums', {
-    headers: { 'Content-Type': 'application/json' },
-    json: true,
-    auth: { 'bearer': access_token },
-  }).then((result) => {
-    console.log(result);
-
-    // result.albums is an array of albums
-    // each album object
-    //    id: string
-    //    mediaItemsCount: string
-    //    title: string
-    //    productUrl: string
-    //      not clear what would be returned using this url
-
-
-  });
 
 }
