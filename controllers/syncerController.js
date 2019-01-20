6d5baa6a3c1c08361236ff04803a29a7a1ab7480
Ted Shaffer <shaffer.family@gmail.com>
2019-01-20 15:14:42 -0800

    don't add items to db if they are already in db.

--- controllers/syncerController.js
+++ controllers/syncerController.js
@@ -7,84 +7,97 @@ var MediaItem = require('../models/mediaItem');
 exports.startSync = function (request, response, next) {
   response.render('syncer');
 
-  var access_token = oauth2Controller.getAccessToken();
-  console.log('start sync process');
-  console.log(access_token);
+  // get list of photo's currently in db
+  var mediaItemIdsInDb;
 
-  const apiEndpoint = 'https://photoslibrary.googleapis.com';
+  MediaItem.find(null, 'id', (err, cloudMediaItems) => {
+    mediaItemIdsInDb = cloudMediaItems.map((cloudMediaItem) => {
+      return cloudMediaItem.id;
+    })
 
-  console.log('invoke: ', apiEndpoint + '/v1/mediaItems');
+    var access_token = oauth2Controller.getAccessToken();
+    console.log('start sync process');
+    console.log(access_token);
 
-  var totalNumberOfMediaItems = 0;
-  var processGetMediaFiles = (pageToken) => {
-    var url = apiEndpoint + '/v1/mediaItems?pageSize=100'
-    if (pageToken !== '') {
-      url = url + '&pageToken=' + pageToken;
-    }
-    requestPromise.get(url, {
-      headers: { 'Content-Type': 'application/json' },
-      json: true,
-      auth: { 'bearer': access_token },
-    }).then((result) => {
+    const apiEndpoint = 'https://photoslibrary.googleapis.com';
 
-      console.log(result.mediaItems.length);
-      console.log(result.nextPageToken);
+    console.log('invoke: ', apiEndpoint + '/v1/mediaItems');
 
-      if (result.mediaItems.length === 0 || result.nextPageToken === undefined) {
-        console.log('retrieved all mediaItems');
-        debugger;
+    var totalNumberOfMediaItems = 0;
+    var processGetMediaFiles = (pageToken) => {
+      var url = apiEndpoint + '/v1/mediaItems?pageSize=100'
+      if (pageToken !== '') {
+        url = url + '&pageToken=' + pageToken;
       }
-      else {
-        totalNumberOfMediaItems = totalNumberOfMediaItems + result.mediaItems.length;
-        console.log('running total is: ', totalNumberOfMediaItems);
+      requestPromise.get(url, {
+        headers: { 'Content-Type': 'application/json' },
+        json: true,
+        auth: { 'bearer': access_token },
+      }).then((result) => {
 
-        var mediaItems = [];
-        for (var i = 0; i < result.mediaItems.length; i++) {
-          var downloadedMediaItem = result.mediaItems[i];
-          var creationTime = null;
-          var width = null;
-          var height = null;
-          if (typeof downloadedMediaItem.mediaMetadata === 'object') {
-            var mediaMetadata = downloadedMediaItem.mediaMetadata;
-            if (typeof mediaMetadata.creationTime === 'string') {
-              creationTime = new Date(mediaMetadata.creationTime);
-            }
-            if (typeof mediaMetadata.width === 'string') {
-              width = Number(mediaMetadata.width);
-            }
-            if (typeof mediaMetadata.height === 'string') {
-              width = Number(mediaMetadata.height);
+        console.log(result.mediaItems.length);
+        console.log(result.nextPageToken);
+
+        if (result.mediaItems.length === 0 || result.nextPageToken === undefined) {
+          console.log('retrieved all mediaItems');
+          debugger;
+        }
+        else {
+          totalNumberOfMediaItems = totalNumberOfMediaItems + result.mediaItems.length;
+          console.log('running total is: ', totalNumberOfMediaItems);
+
+          var mediaItems = [];
+          for (var i = 0; i < result.mediaItems.length; i++) {
+            var downloadedMediaItem = result.mediaItems[i];
+
+            const id = downloadedMediaItem.id;
+            if (mediaItemIdsInDb.indexOf(id) < 0) {
+              var creationTime = null;
+              var width = null;
+              var height = null;
+              if (typeof downloadedMediaItem.mediaMetadata === 'object') {
+                var mediaMetadata = downloadedMediaItem.mediaMetadata;
+                if (typeof mediaMetadata.creationTime === 'string') {
+                  creationTime = new Date(mediaMetadata.creationTime);
+                }
+                if (typeof mediaMetadata.width === 'string') {
+                  width = Number(mediaMetadata.width);
+                }
+                if (typeof mediaMetadata.height === 'string') {
+                  width = Number(mediaMetadata.height);
+                }
+              }
+              var mediaItem = {
+                id,
+                baseUrl: downloadedMediaItem.baseUrl,
+                fileName: downloadedMediaItem.filename,
+                productUrl: downloadedMediaItem.productUrl,
+                mimeType: downloadedMediaItem.mimeType,
+                creationTime,
+                width,
+                height,
+              };
+              mediaItems.push(mediaItem);
             }
           }
-          var mediaItem = {
-            id: downloadedMediaItem.id,
-            baseUrl: downloadedMediaItem.baseUrl,
-            fileName: downloadedMediaItem.filename,
-            productUrl: downloadedMediaItem.productUrl,
-            mimeType: downloadedMediaItem.mimeType,
-            creationTime,
-            width,
-            height,
-          };
-          mediaItems.push(mediaItem);
+          var promise = MediaItem.insertMany(mediaItems);
+          promise
+            .then((promiseResults) => {
+              console.log('all media items added to db');
+              processGetMediaFiles(result.nextPageToken);
+            })
+            .catch((err) => {
+              console.log(err);
+              debugger;
+            });
         }
-        var promise = MediaItem.insertMany(mediaItems);
-        promise
-        .then( (promiseResults) => {
-          console.log('all media items added to db');
-          processGetMediaFiles(result.nextPageToken);
-        })
-        .catch( (err) => {
-          console.log(err);
-          debugger;
-        });
-      }
-    });
-  };
+      });
+    };
 
-  processGetMediaFiles('');
+    processGetMediaFiles('');
+  });
 
-    // var downloadedFile = result.mediaItems[0];
+  // var downloadedFile = result.mediaItems[0];
 
   // requestPromise.get(apiEndpoint + '/v1/albums', {
   //   headers: { 'Content-Type': 'application/json' },
@@ -103,6 +116,5 @@ exports.startSync = function (request, response, next) {
 
 
   // });
-
 }
 
