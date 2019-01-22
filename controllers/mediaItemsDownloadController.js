66aff93bd7df220234e370d7e221cd3541c738b5
Ted Shaffer <shaffer.family@gmail.com>
2019-01-22 13:02:16 -0800

    given google media item id, retrieve google media item. get the current baseUrl from the populated media item. With that and the width / height, construct the url. download the photo.

--- controllers/mediaItemsDownloadController.js
+++ controllers/mediaItemsDownloadController.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 const axios = require('axios')
+const requestPromise = require('request-promise');
+var oauth2Controller = require('./oauth2Controller');
 
 var MediaItem = require('../models/mediaItem');
 var pug = require('pug');
@@ -26,10 +28,10 @@ exports.downloadMediaItems = function (request, response) {
         };
         ws.send(JSON.stringify(messageData));
 
-        MediaItem.find({ 'downloaded': false }, 'id productUrl baseUrl fileName', (err, cloudMediaItems) => {
+        MediaItem.find({ 'downloaded': false }, 'id fileName', (err, cloudMediaItems) => {
             messageData = {
                 type: 'OverallStatus',
-                data: 'Remaining media items to download: ' + cloudMediaItems.length.toString() 
+                data: 'Remaining media items to download: ' + cloudMediaItems.length.toString()
             };
             ws.send(JSON.stringify(messageData));
             // ws.send('Remaining media items to download: ' + cloudMediaItems.length.toString());
@@ -40,17 +42,17 @@ exports.downloadMediaItems = function (request, response) {
                     console.log('downloaded mediaItem: ', mediaItem.fileName);
 
                     // update db - set downloaded flag to true
-                    mediaItem.set( { downloaded: true });
-                    mediaItem.save( (err, updatedMediaItem) => {
+                    mediaItem.set({ downloaded: true });
+                    mediaItem.save((err, updatedMediaItem) => {
                         if (err) debugger;
                         console.log(updatedMediaItem);
                     });
 
                     messageData = {
                         type: 'ProgressItem',
-                        data: 'Downloaded mediaItem: ' + mediaItem.fileName 
+                        data: 'Downloaded mediaItem: ' + mediaItem.fileName
                     };
-                    ws.send(JSON.stringify(messageData));        
+                    ws.send(JSON.stringify(messageData));
                     cloudMediaItemIndex = cloudMediaItemIndex + 1;
                     if (cloudMediaItemIndex > 9) {
                         debugger;
@@ -61,7 +63,7 @@ exports.downloadMediaItems = function (request, response) {
                     debugger;
                 });
             }
-            
+
             executeDownloadMediaItems(0);
         });
     })
@@ -74,27 +76,61 @@ function downloadMediaItem(mediaItem) {
 
     return new Promise((resolve, reject) => {
 
-        var url = mediaItem.baseUrl;
-        console.log('baseUrl');
-        console.log(url);
-        console.log(url);
+        var id = mediaItem.id;
 
-        var fileName = mediaItem.fileName;
-        console.log('filename');
-        console.log(fileName);
+        const apiEndpoint = 'https://photoslibrary.googleapis.com';
+        var getMediaItemEndpoint = apiEndpoint + '/v1/mediaItems/' + id;
+        console.log('mediaItems endpoint:');
+        console.log(getMediaItemEndpoint);
 
-        const filePath = path.join(baseDir, fileName);
+        var access_token = oauth2Controller.getAccessToken();
 
-        const writer = fs.createWriteStream(filePath)
+        requestPromise.get(getMediaItemEndpoint, {
+            headers: { 'Content-Type': 'application/json' },
+            json: true,
+            auth: { 'bearer': access_token },
+        }).then((result) => {
 
-        axios({
-            method: 'get',
-            url,
-            responseType: 'stream'
-        }).then((response) => {
-            response.data.pipe(writer);
-            writer.on('finish', resolve)
-            writer.on('error', reject)
+            const populatedMediaItem = result;
+            var baseUrl = populatedMediaItem.baseUrl;
+            console.log('baseUrl: ');
+            console.log(baseUrl);
+
+            if (typeof populatedMediaItem.mediaMetadata === 'object') {
+                var mediaMetadata = populatedMediaItem.mediaMetadata;
+                if (typeof mediaMetadata.width === 'string' && typeof mediaMetadata.height === 'string') {
+                    width = mediaMetadata.width;
+                    height = mediaMetadata.height;
+                    baseUrl = baseUrl + '=w' + width + '-h' + height;
+                    console.log('baseUrl with dimensions:');
+                    console.log(baseUrl);
+                }
+            }
+0
+            // populatedMediaItem.mediaMetadata.width, height
+
+            // TODO - can't really do this - some are movies
+            var fileName = mediaItem.id + '.jpg';
+            console.log('filename');
+            console.log(fileName);
+
+            const filePath = path.join(baseDir, fileName);
+
+            const writer = fs.createWriteStream(filePath)
+
+            axios({
+                method: 'get',
+                url: baseUrl,
+                responseType: 'stream'
+            }).then((response) => {
+                response.data.pipe(writer);
+                writer.on('finish', resolve)
+                writer.on('error', reject)
+            }).catch((err) => {
+                debugger;
+            });
+        }).catch((err) => {
+            debugger;
         });
     });
 }
